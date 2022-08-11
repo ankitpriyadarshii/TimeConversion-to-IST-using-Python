82996e38cab9b773326d87d0e03c4c9b2f9ff686
ANKIT PRIYADARSHI <72643034+ankitpriyadarshii@users.noreply.github.com>
2022-08-11 10:59:15 +0530

    Add files via upload

---
+++ TimeConversion.py
@@ -0,0 +1,58 @@
+print("Enter the Time in 24hrs format\nHours: ", end="")
+hrs = int(input())
+print("Minutes: ", end="")
+mnt = int(input())
+print("\nSelect your choice: ")
+print("1. UTC\n2. UTC -4\n3. UTC +8\n4. GMT\n5. GMT +8\n")
+ch = int(input(""))
+print("IST time")
+if ch == 1:
+    hrs = hrs+5
+    mnt = mnt+30
+    if mnt > 59:
+        mnt = mnt - 60
+        hrs += 1
+    if hrs > 23:
+        print("Next Day")
+        hrs = hrs - 24
+    print(hrs, ":", mnt)
+elif ch == 2:
+    hrs = hrs+9
+    mnt = mnt+30
+    if mnt > 59:
+        mnt = mnt - 60
+        hrs += 1
+    if hrs > 23:
+        print("Next Day")
+        hrs = hrs - 24
+    print(hrs, ":", mnt)
+elif ch == 3:
+    hrs = hrs-3
+    mnt = mnt+30
+    if mnt > 59:
+        mnt = mnt - 60
+        hrs += 1
+    if hrs < 0:
+        print("Previous Date")
+        hrs = 24 + hrs
+    print(hrs, ":", mnt)
+elif ch == 4:
+    hrs = hrs+5
+    mnt = mnt+30
+    if mnt > 59:
+        mnt = mnt - 60
+        hrs += 1
+    if hrs > 23:
+        print("Next Day")
+        hrs = hrs - 24
+    print(hrs, ":", mnt)
+elif ch == 5:
+    hrs = hrs-3
+    mnt = mnt+30
+    if mnt > 59:
+        mnt = mnt - 60
+        hrs += 1
+    if hrs < 0:
+        print("Previous Date")
+        hrs = 24 + hrs
+    print(hrs, ":", mnt)
